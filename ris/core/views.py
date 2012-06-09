ffdd7844b16fbe18af954c39d8ed98c8a13e5975
Chris Turner <christopherjturner@gmail.com>
2012-06-09 12:01:43 +0100

    flask principal login groudnwork. see /test/login

--- ris/core/views.py
+++ ris/core/views.py
@@ -1,18 +1,42 @@
-from flask import request, render_template, url_for, redirect, flash
+from wtforms import Form, BooleanField, TextField, PasswordField, validators, DateField
+from flask import current_app, request, render_template, url_for, redirect, flash
+from flask.ext.principal import identity_changed, Identity, AnonymousIdentity
 from sqlalchemy import func
-from models import Patient, Address
+from ris.permissions import admin_permission, user_permission
 from ris.database import db
 from ris.core import bp
-from wtforms import Form, BooleanField, TextField, PasswordField, validators, DateField
+from models import Patient, Address
 
 @bp.after_request
 def shutdown_session(response):
 	db.session.remove()
 	return response
 
+@bp.route('/test/login', methods=("GET","POST"))
+def test_login():
+	form = LoginForm(request.form)
+	if request.method == 'POST' and form.validate():
+		username = form.username.data
+		password = form.password.data
+
+		# todo: do real authentication here
+		if password == 'password':
+			identity_changed.send(current_app._get_current_object(), identity=Identity(username))
+			return redirect(url_for('.test'))
+		else:
+			return 'wrong password. get out %s' % (password)
+	else:
+		return render_template('testlogin.html', form=form)
+
 @bp.route('/test')
+@admin_permission.require(401)
 def test():
 	return render_template('formtest.html')
+
+@bp.route('/test/user')
+@user_permission.require(401)
+def test_userpermission():
+	return 'you are a valid user. well done.'
 
 @bp.route('/register', methods=['GET','POST'])
 def register_patient():
@@ -89,3 +113,7 @@ class PatientDemographicsForm(Form):
 	address_line4 = TextField('Address Line 4' )
 	post_code = TextField('Post Code')
 
+
+class LoginForm(Form):
+	username = TextField('Username')
+	password = PasswordField('Password')
